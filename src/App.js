0a1ed307407c3fffe31ab555effc5a7c165b8294
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-02-08 20:25:48 +0500

    vazifa tayyor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">
+          <h1>Welcome to our web site</h1>
+          </div>    
       </header>
+      <div className="main-card">
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Uzbekistan</h2>
+          <h4>Poppulation: <span>36.780.682</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Tashkent</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Tojikiston</h2>
+          <h4>Poppulation: <span>27.780.682</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Dushanbe</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Xitoy</h2>
+          <h4>Poppulation: <span>1.436.780.682</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Pekin</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Yaponiya</h2>
+          <h4>Poppulation: <span>186.625.402</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Tokio</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Rossiya</h2>
+          <h4>Poppulation: <span>306.120.602</span></h4>
+          <h4>Region: <span>Yevrosiyo</span></h4>
+          <h4>Capital: <span>Moskva</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Uzbekistan</h2>
+          <h4>Poppulation: <span>36.780.682</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Tashkent</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Qosig'iston</h2>
+          <h4>Poppulation: <span>22.780.682</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Nur-Sulton</span></h4>
+        </div>
+        <div className="card">
+          <img src="https://picsum.photos/seed/picsum/250/200"></img>
+          <h2>Qirg'iston</h2>
+          <h4>Poppulation: <span>6.590.682</span></h4>
+          <h4>Region: <span>Asia</span></h4>
+          <h4>Capital: <span>Bishkek</span></h4>
+        </div>
+
+      </div>
     </div>
   );
 }
